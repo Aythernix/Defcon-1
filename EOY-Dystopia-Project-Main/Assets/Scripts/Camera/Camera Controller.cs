55ff0972a6287a842914c59b6903d8945b3b4d90
Ayther <121729473+Aythernix@users.noreply.github.com>
2025-01-26 19:55:29 +0000

    Initial Commit

---
+++ EOY-Dystopia-Project-Main/Assets/Scripts/Camera/Camera Controller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Controls.CCTVCameraActions _input;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       _input = InputManager.current.inputs.CCTVCamera;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Vector2 input = _input.CameraMovement.ReadValue<Vector2>();
+        float rotationSpeed = 25f; // Adjust the speed as needed
+
+        if (input.x != 0)
+        {
+            float currentY = gameObject.transform.eulerAngles.y;
+            float newRotationY = Mathf.Clamp((currentY > 180 ? currentY - 360 : currentY) + input.x * rotationSpeed * Time.deltaTime, -45f, 45f);
+            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, newRotationY, gameObject.transform.eulerAngles.z);
+        }
+        Debug.Log(input);
+
+
+        Debug.Log(_input.CameraMovement.ReadValue<Vector2>());
+    }
+}
