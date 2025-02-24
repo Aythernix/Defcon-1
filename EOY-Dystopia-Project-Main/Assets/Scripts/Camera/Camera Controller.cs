c04952cfc3f78cf414b836e0b75ef86f719ef9e9
Ayther <121729473+Aythernix@users.noreply.github.com>
2025-02-24 22:06:11 +0000

    Prototype 0.0.2
    Added basic firing mechanics.

--- EOY-Dystopia-Project-Main/Assets/Scripts/Camera/Camera Controller.cs
+++ EOY-Dystopia-Project-Main/Assets/Scripts/Camera/Camera Controller.cs
@@ -1,32 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using Input = UnityEngine.Windows.Input;
 
 public class CameraController : MonoBehaviour
 {
-    private Controls.CCTVCameraActions _input;
+       private Controls.CCTVCameraActions _Controls;
+       
+       private Vector2 _input;
+    
+    [Header("CameraControls")]
+    
+    [SerializeField]
+    private float _cameraMoveSpeed = .8f;
+    
+    [Header("GunControls")]
+    
+    [SerializeField]
+    private GameObject firePoint;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    [SerializeField]
+    private float _Range = 100f;
+    
+    
+    
+ 
+    
     
     // Start is called before the first frame update
     void Start()
     {
-       _input = InputManager.current.inputs.CCTVCamera;
+       _Controls = InputManager.current.inputs.CCTVCamera;
+       
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        #region Controls
         
+        _input = _Controls.CameraMovement.ReadValue<Vector2>();
 
-        Vector2 input = _input.CameraMovement.ReadValue<Vector2>();
-        float rotationSpeed = 25f; // Adjust the speed as needed
+        #endregion
 
-        if (input.x != 0)
+
+
+        #region Camera Movement
+        
+        if (_input.x != 0 || _input.y != 0)
         {
-            float currentY = gameObject.transform.eulerAngles.y;
-            float newRotationY = Mathf.Clamp((currentY > 180 ? currentY - 360 : currentY) + input.x * rotationSpeed * Time.deltaTime, -60f, 60f);
-            gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, newRotationY, gameObject.transform.eulerAngles.z);
+            CameraMovement();
+            Debug.Log(_input);
         }
-        Debug.Log(input);
-        
+
+        #endregion
+       
+        if (UnityEngine.Input.GetMouseButton(0))
+        {
+            Firing();
+        }
+    }
+
+    private void CameraMovement()
+    {
+        float currentY = gameObject.transform.eulerAngles.y;
+        float newRotationY = Mathf.Clamp((currentY > 180 ? currentY - 360 : currentY) + _input.x * _cameraMoveSpeed, -60f, 60f);
+
+        float currentX = gameObject.transform.eulerAngles.x;
+        float newRotationX = Mathf.Clamp((currentX > 180 ? currentX - 360 : currentX) + _input.y * _cameraMoveSpeed, -60f, 60f);
+
+        gameObject.transform.eulerAngles = new Vector3(newRotationX, newRotationY, gameObject.transform.eulerAngles.z);
+    }
+    
+    private void Firing()
+    {
+        Physics.Raycast(firePoint.transform.position, firePoint.transform.forward, out RaycastHit hit, _Range, LayerMask.GetMask("Enemy"));
+        hit.transform.gameObject.SetActive(false);
     }
 }
