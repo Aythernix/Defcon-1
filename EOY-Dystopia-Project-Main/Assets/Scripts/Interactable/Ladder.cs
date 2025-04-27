b9361ebc1c645a45ee4a561b0d0e7d9948495ce5
Ayther <121729473+Aythernix@users.noreply.github.com>
2025-04-27 18:11:52 +0100

    0.9.10 - Added ladder system

---
+++ EOY-Dystopia-Project-Main/Assets/Scripts/Interactable/Ladder.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour, IInteractable
+{
+    public string InteractText => "Climb Ladder";
+    
+    [Header("Ladder Config")]
+    [SerializeField] private Transform _ladderTop;
+    [SerializeField] private Transform _ladderBottom;
+
+    public bool Interact(Interactor interactor)
+    {
+        if (Vector3.Distance(interactor.transform.position, _ladderTop.position) > Vector3.Distance(interactor.transform.position, _ladderBottom.position))
+        {
+            interactor.GetComponent<CharacterController>().enabled = false;
+            interactor.gameObject.transform.position = _ladderTop.position;
+            interactor.GetComponent<CharacterController>().enabled = true;
+            Debug.Log("went top");
+        }
+        else
+        {
+            interactor.GetComponent<CharacterController>().enabled = false;
+            interactor.gameObject.transform.position = _ladderBottom.position;
+            interactor.GetComponent<CharacterController>().enabled = true;
+            Debug.Log("went bottom");   
+        }
+        
+        return true;
+    }
+}
+    
